ac3d27db773aebb74c31a88a01dce9183d1bcdfd
Navkaran Bindra <navsbindra@gmail.com>
2022-04-09 18:07:05 -0400

    Plaintext

--- wikipy/Plaintext.py
+++ wikipy/Plaintext.py
@@ -1,4 +1,25 @@
+# %%
+import re
+
+# %%
 class Plaintext:
     def __init__(self, text):
-        self.text = text
-        self.titles = list()
+        self.title = re.findall(r'\A(.*)From Wikipedia', text)
+        self.title = self.title[0].strip().replace(' ','_')
+        self.titles = re.findall(r'\|\|(\S+)\|\|', text)
+        self.text = text.replace("||","")
+# %%
+file_name = "../CleanPages/World_War_II.txt"
+text_file = open(file_name, "r")
+text = text_file.read()
+
+
+pt = Plaintext(text)
+
+# %%
+pt.title
+# %%
+pt.titles
+# %%
+pt.text
+# %%
