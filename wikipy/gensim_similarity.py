f311efff78ca1d672e0dfdf473173d6e2d230da0
landrumb <8enlandrum@gmail.com>
2022-04-09 16:07:57 -0400

    working title comparison model

--- wikipy/gensim_similarity.py
+++ wikipy/gensim_similarity.py
@@ -12,7 +12,7 @@ word_vectors = gensim.models.KeyedVectors.load_word2vec_format(MODEL, binary=Tru
 # %%
 print(word_vectors.most_similar("china_PROPN"))
 # %%
-wiki_vectors = gensim.downloader.download('fasttext-wiki-news-subwords-300')
+# wiki_vectors = gensim.downloader.download('fasttext-wiki-news-subwords-300')
 # %%
 wiki_vectors.most_similar("china")
 # %%
@@ -23,3 +23,19 @@ def most_similar(model, word):
 # %%
 most_similar(word_vectors, 'china')
 # %%
+MODEL_2 = "wiki-news-300d-1M.vec"
+
+news_vectors = gensim.models.KeyedVectors.load_word2vec_format(MODEL_2)
+# %%
+news_vectors.n_similarity(["Einstein"], ["Kardashian", "Kim"])
+# %%
+def rank_similarity(model, target, terms):
+    """returns a sorted list of (word, similarity) tuples.
+    
+    terms should be underscore separated titles
+    """
+    output = dict()
+    for title in terms:
+        output[title] = model.n_similarity(target, title.split('_'))
+
+    return sorted(output.items(), key=lambda x: x[1], reverse=True)
