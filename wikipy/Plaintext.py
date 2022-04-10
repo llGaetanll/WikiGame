d5dc91faa313ea67edf0a238cf427d82218b86ae
Navkaran Bindra <navsbindra@gmail.com>
2022-04-10 02:45:44 -0400

    whole lotta shit

--- wikipy/Plaintext.py
+++ wikipy/Plaintext.py
@@ -6,7 +6,7 @@ class Plaintext:
     def __init__(self, text):
         self.title = re.findall(r'\A(.*)From Wikipedia', text)
         self.title = self.title[0].strip().replace(' ','_')
-        self.articles = re.findall(r'\|\|(\S+)\|\|', text)
+        self.links = re.findall(r'\|\|(\S+)\|\|', text)
         self.text = text.replace("||","")
 # %%
 file_name = "../CleanPages/World_War_II.txt"
@@ -19,6 +19,6 @@ pt = Plaintext(text)
 # %%
 pt.title
 # %%
-pt.articles
+pt.links
 # %%
 pt.text
